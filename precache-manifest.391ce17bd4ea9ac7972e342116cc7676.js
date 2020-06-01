cf8c5db407a6075adc6cfa038004c3ff0dc5b5e4
Prashant Singh <ps.prashantsingh.98@gmail.com>
2020-06-01 16:52:33 +0530

    Updates

---
+++ precache-manifest.391ce17bd4ea9ac7972e342116cc7676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c88750b6574f0fb4c9a11744e18b1b7",
+    "url": "/ToDo-List-app/index.html"
+  },
+  {
+    "revision": "4a11762196179fe38aca",
+    "url": "/ToDo-List-app/static/css/main.44546874.chunk.css"
+  },
+  {
+    "revision": "814206c61b9de9acd889",
+    "url": "/ToDo-List-app/static/js/2.5a98ad57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List-app/static/js/2.5a98ad57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a11762196179fe38aca",
+    "url": "/ToDo-List-app/static/js/main.71ac9e8f.chunk.js"
+  },
+  {
+    "revision": "5a3121536f47ee52bb61",
+    "url": "/ToDo-List-app/static/js/runtime-main.2d6ccfca.js"
+  },
+  {
+    "revision": "c2d7f2c485c776a68a2f91f35bfe23cd",
+    "url": "/ToDo-List-app/static/media/aklogo.c2d7f2c4.jpg"
+  }
+]);
